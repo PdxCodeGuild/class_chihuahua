6fcfc4b479322e72038493a80e8f53ed39161952
No Go <isabel.coverdale@yahoo.com>
2022-09-12 23:18:51 -0500

    v3 continued

--- code/isabel/Python/unit_converter_lab/unit_converter_3.py
+++ code/isabel/Python/unit_converter_lab/unit_converter_3.py
@@ -1,3 +1,6 @@
+from lib2to3.pytree import convert
+
+
 def unit_convert_units():
     data = input("Please enter a number: ")
     data_units = input("Please enter units (m, km, mi, ft, yd, in) of number: ")
@@ -42,7 +45,35 @@ def unit_convert_units():
         elif data_units == 'mi' and convert_units == 'ft':
             answer = data_flo * (1609.34/0.3048)
             return f"{answer} ft"
-
+        elif data_units == 'yd' and convert_units == 'm':
+            answer = data_flo * 0.9144
+            return f"{answer} m"
+        elif data_units == 'm' and convert_units == 'yd':
+            answer = data_flo * (1/0.9144)
+            return f"{answer} yd"
+        elif data_units == 'ft' and convert_units == 'yd':
+            answer = data_flo * (12 * 0.0254)
+            return f"{answer} yd"
+        elif data_units == 'yd' and convert_units == 'ft':
+            answer = data_flo * (1/(12 * 0.0254))
+            return f"{answer} ft"
+        elif data_units == 'yd' and convert_units == 'mi':
+            answer = data_flo * (0.9144/1609.34)
+            return f"{answer} mi"
+        elif data_units == 'mi' and convert_units == 'yd':
+            answer = data_flo * (1609.34/0.9144)
+            return f"{answer} yd"
+        elif data_units == 'yd' and convert_units == 'in':
+            answer = data_flo * (0.9144/0.0254)
+            return f"{answer} in"
+        elif data_units == 'in' and convert_units == 'yd':
+            answer = data_flo * (0.0254/0.9144)
+        elif data_units == 'yd' and convert_units == 'km':
+            answer = data_flo * (0.9144/1000)
+            return f"{answer} km"
+        elif data_units == "km" and convert_units == "yd":
+            answer = data_flo * (1000/0.9144)
+            return f"{answer} yd"
 
 
 
