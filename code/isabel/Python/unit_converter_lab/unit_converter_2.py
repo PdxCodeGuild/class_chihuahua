dbfb075539fb41c0edeef98499b48a640d7dcdc8
No Go <isabel.coverdale@yahoo.com>
2022-09-12 22:27:31 -0500

    version 2 working

---
+++ code/isabel/Python/unit_converter_lab/unit_converter_2.py
@@ -0,0 +1,25 @@
+def unit_convert_units():
+    data = input("Please enter a number: ")
+    data_units = input("Please enter units (m, km, mi, ft) of number: ")
+    convert_units = input("Please enter units to convert to: ")
+    data_flo = float(data)
+
+    if data_units == convert_units:
+        return data_flo + data_units
+    else:
+
+        if data_units == 'ft' and convert_units == 'm':
+            answer = data_flo * 0.3048
+            return f"{answer} + m"
+        elif data_units == 'ft' and convert_units == 'mi':
+            answer = data_flo * (0.3048/1609.34)
+            return f"{answer} + mi"
+        elif data_units == 'ft' and convert_units == 'km':
+            answer =  data_flo * (0.3048/1000)
+            return f"{answer} + km"
+
+
+unit_convert_units()
+print(unit_convert_units())
+        
+
