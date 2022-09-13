e8e2f3110152d2912f8852adcc3572af672354fe
JTWATTZ <thisisinsane4what@outlook.com>
2022-09-13 06:21:26 +0200

    version2 had part of verson3 and was removed

--- code/JTWATTS/PYTHON/unit_vonverter_2.py
+++ code/JTWATTS/PYTHON/unit_vonverter_2.py
@@ -10,9 +10,9 @@ distance = {
 def unit_converter():
     conversion = input("what is the input units?  ")
     how_far = int(input('what is the distance?  '))
-    output_unit = input("what are the output units? ")
+    #output_unit = input("what are the output units? ")
     answer = how_far * distance[conversion] 
-    print(f' {how_far} {conversion} is {answer} {output_unit}')
+    print(f' {how_far} {conversion} is {answer} m')
     
     # if conversion == mi:
     #     answer1 = how_far * 1609.34
