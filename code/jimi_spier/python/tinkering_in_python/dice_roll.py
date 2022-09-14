04a430b739ad8aebe325c06c6090bd84d164b7a9
autobotJimi <jimi.spier@gmail.com>
2022-09-14 12:02:30 -0700

    changed position of esc char on line 26

--- code/jimi_spier/python/tinkering_in_python/dice_roll.py
+++ code/jimi_spier/python/tinkering_in_python/dice_roll.py
@@ -22,6 +22,6 @@ while True: #keep going until manually stopped by condition
         dice_roll = random.randint(1, user_number_int) # inserts user_number_int into randint and stores in dice_roll
         user_results.append(dice_roll) #Appends a new result to user_results on each interation
         # -------------Final-Result-------------------------------------- #
-        print(f"Your {user_number_int} sided die resulted in: {dice_roll} \n") #Gives original sided die and result
-        print(f"Your results so far are: {user_results}") #Prints a tally of rolls by user
+        print(f"Your {user_number_int} sided die resulted in: {dice_roll}") #Gives original sided die and result
+        print(f"Your results so far are: {user_results}\n") #Prints a tally of rolls by user
     
