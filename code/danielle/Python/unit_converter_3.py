39827e6da3a6ed2ec247e853f9af95471c4d76aa
Dani Cantu <daniellecantu@dovahkiin.myfiosgateway.com>
2022-09-14 00:18:45 -0400

    updating version 2 and 3

--- code/danielle/Python/unit_converter_3.py
+++ code/danielle/Python/unit_converter_3.py
@@ -1,11 +1,15 @@
-unit_converter = {
-    "ft": .3048,
-    "mi": 1609.34,
-    "km": 1000,
-    "yd": .9144,
-}
-distance = input("What is the distance?: ")
-distance = int(distance)
-units = input("What are the units? (ft, mi, km, yd, in): ")
+def unit_converter():
+    initial_value = int(input("what is the distance?: "))
+    initial_units = input("What are the units? (ft, mi, km, yd, in): ")
+    if initial_units == 'ft':
+        print(f"{initial_value} {initial_units} is {.3048 * initial_value} m")
+    elif initial_units == 'mi':
+        print(f"{initial_value} {initial_units} is {1609.34 * initial_value} m")
+    elif initial_units == 'km':
+        print(f"{initial_value} {initial_units} is {1000 * initial_value} m")
+    elif initial_units == 'yd':
+        print(f"{initial_value} {initial_units} is {.9144 * initial_value} m")
+    elif initial_units == 'in':
+        print(f"{initial_value} {initial_units} is {.0254 * initial_value} m")
 
-print(f"{distance} {units} is {round(unit_converter[units] * distance, 4)} m")
+unit_converter()
