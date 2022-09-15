d1f1892bde8e47fe8629e7f9508e86c63a181970
User <user@iMac.local>
2022-09-14 21:29:46 -0700

    num to phrase work in progress

---
+++ code/alexander/python/num_to_phrase/num_to_phrase_1.py
@@ -0,0 +1,52 @@
+
+def num_to_phrase(num):
+    numbers = {
+    0: "",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen"
+    }
+    tens_digit = {
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+    100: "one hundred"
+    }
+    if num <= 20:
+            return numbers
+    elif num <= 100:
+        return (num//10) + (num)
+    tens_digit = num//10
+        
+    ones_digit = num%10
+    final_number_in_words = numbers[tens_digit] + numbers[ones_digit]
+    return final_number_in_words
+
+print(num_to_phrase(5))
+'''
+x = 67
+tens_digit = x//10
+ones_digit = x%10
+
+'''
