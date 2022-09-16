31d72116a08285b1d27a98f7f14d7581b68fd9c3
Rachel Lindsay <rachellindsay@Rachels-MacBook-Pro.local>
2022-09-15 21:16:13 -0700

    v1, v2

---
+++ code/rachel/python/number_to_phrase/number_to_phrase_v3.py
@@ -0,0 +1,42 @@
+# convert a number to roman numerals
+# use a list of dictionaries so that the index positions can relate to the first, second, and third digits
+
+'''
+roman numerals
+
+1 = I
+2 = II
+3 = III
+4 = IV
+5 = V
+6 = VI
+7 = VII
+8 = VIII
+9 = IX
+10 = X
+11 = XI
+12 = XII
+13 = XIII
+14 = XIV
+15 = XV
+16 = XVI
+17 = XVII
+18 = XVIII
+19 = XIX
+20 = XX
+
+50 = L
+100 = C
+500 = D
+1000 = M
+
+1999 = MCMXCIX
+1971 = MCMLXXI
+1492 = MCDXCII
+
+
+
+
+
+
+'''
