19f81395bd16541adcb9277348797d705e8dc4d7
hieul98 <hieule1998@yahoo.com>
2022-09-16 21:31:28 -0700

    renamed test file for num to phrase 2, and continued the program, still not complete

--- code/Hieu_L/python/num_to_phrase_2.py
+++ code/Hieu_L/python/num_to_phrase_2.py
@@ -2,8 +2,8 @@ from re import M
 
 
 def num_to_phrase(num):
-    numbers_class = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight'}
+    numbers_class = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten'}
     return numbers_class[num] 
 pass
    
-print(num_to_phrase(2))
+print(num_to_phrase(8))
