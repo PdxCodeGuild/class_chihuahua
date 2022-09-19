5b4445682d65a2c10cf84bff74f09ca1791f7f10
JTWATTZ <thisisinsane4what@outlook.com>
2022-09-19 06:31:40 +0200

    Lab8v1

--- code/JTWATTS/PYTHON/lab_8.py
+++ code/JTWATTS/PYTHON/lab_8.py
@@ -1,25 +1,46 @@
-from multiprocessing.connection import answer_challenge
-import random
-
+import random    
+#x is the random number
 x = random.randint(1,10)
-print(x)
-
-guess = input("guess the number: ")
-# guess = []
-# counts = enumerate(guess)
-tally = []
-tally = int(tally)
-while guess != x and < 10:
-
-    print(guess)
+# print(x)
+# we have to define guess before we can place it in our while loop
+guess = 0
+# we have to define tally before we can place it in our while loop.  Spent too much time trying to make a list with an interger
+tally = 0
+#while loop 
+while guess != x and tally < 10:
+    # x = random.randint(1,10)
+    # print(x)
+    #now that guess and tally have been defined, we can take a users input and place it in guess
+    guess = int(input("guess the number: "))
+    # print(tally)
+    #we take each time the question is asked and add it to the tally
+    tally += 1
+    if guess == x:
+        print(f"correct you guessed {tally}") 
+        #quit the game with break
+        break
     if guess!= x:
-        tally += 1
-    if tally == 10:
-        print('you failed 10 times')
+        #no break statement because they are not done
+        print("try again")
+     # as stated fail 10 times and print this  
+    if tally ==10:
+        print('you failed 10 times')    
+        #quit the game with break
+        break
+# print(tally)
+# if tally == 10:           
+#     print(x)
+     
 
-        print("try again!")
-        print(x)
-        continue
+# word = "accccddeeegeelopanm"
+# counted = {}
+# for char in word:
+#   if char in counted:
+#     counted[char] += 1
+#   else:
+#     counted[char] = 1
+# print(counted)
+
     # else:
     #     print("correct! you guessed it")  
     #     break  
