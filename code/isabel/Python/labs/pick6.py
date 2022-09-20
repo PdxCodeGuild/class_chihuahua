fbec5d2217ec61d063c5f1632fcba99412032571
No Go <isabel.coverdale@yahoo.com>
2022-09-19 23:17:51 -0500

    working progress pick6

--- code/isabel/Python/labs/pick6.py
+++ code/isabel/Python/labs/pick6.py
@@ -2,7 +2,7 @@ import random
 
 def random_winning():
     winning_ticket = []
-    while len(winning_ticket) < 7:
+    while len(winning_ticket) < 6:
         winning_random_numbers = random.randint(1, 99)
         winning_ticket.append(winning_random_numbers)
     return winning_ticket
@@ -10,7 +10,7 @@ print(random_winning())
 
 def computer_random_ticket():
     computer_ticket = []
-    while len(computer_ticket) < 7:
+    while len(computer_ticket) < 6:
         computer_random_pick = random.randint(1, 99)
         computer_ticket.append(computer_random_pick)
     return computer_ticket
@@ -37,9 +37,26 @@ def gambling_problem():
     }
 
     for key in matching_picks.keys():
-        if compare_tickets() == key:
+        if compare_tickets() == key and compare_tickets != 0:
             winnings = matching_picks[key]
-            return f"You won {winnings}!"
+            print(f"this is how much won {winnings}")
+            return winnings
         else:
-            return "No winner"
+            return 0
 print(gambling_problem())
+
+def thousand_tries():
+    count_tries = 0
+    total_winnings = gambling_problem()
+    while count_tries < 10:
+        gambling_problem()
+        count_tries += 1
+        print(count_tries, gambling_problem(), compare_tickets(), computer_random_ticket())
+        if gambling_problem() != 0:
+            total_winnings += gambling_problem()
+            print(total_winnings())
+        
+
+    return total_winnings()
+    
+print(thousand_tries())
