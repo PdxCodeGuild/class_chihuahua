8586748c547029f5142848edaae7a1e49b0d4fca
User <user@iMac.local>
2022-09-19 21:39:04 -0700

    work in progress on pick6

---
+++ code/alexander/python/pick6/pick_6_v1.py
@@ -0,0 +1,35 @@
+# pick 6 random integers as the 'winning number'
+import random
+# use randint
+def pick6():
+    winning_number = [random.randint(1,100) for num in range(6)]
+    return winning_number
+
+ticket_quantity = 0
+balance = 0
+def ticket():
+    while ticket_quantity < 100,000
+        ticket_generated = [random.randint(1,100) for num in range(6)]
+        ticket_quantity += 1
+        balance -= 2
+        return ticket_generated
+
+def compare(pick6(),ticket()):
+    
+
+# balance at zero
+
+
+def num_matches(pick6,ticket):
+    if pick6 == ticket:
+        return
+    pass
+# ticket cost $2
+'''
+1 number match $4
+2 number match $7
+3 number match $100
+4 number match $50,000
+5 number match $1,000,000
+6 number match $25,000,000
+'''
