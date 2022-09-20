5f1c9c583ae7a91b0470eb121196c3656f5deda2
No Go <isabel.coverdale@yahoo.com>
2022-09-19 22:33:50 -0500

    continued in progress pick6

--- code/isabel/Python/labs/pick6.py
+++ code/isabel/Python/labs/pick6.py
@@ -11,10 +11,35 @@ print(random_winning())
 def computer_random_ticket():
     computer_ticket = []
     while len(computer_ticket) < 7:
-        computer_random_pick = random.int(1, 99)
+        computer_random_pick = random.randint(1, 99)
         computer_ticket.append(computer_random_pick)
     return computer_ticket
+print(computer_random_ticket())
 
 def compare_tickets():
+    i = 0
     for i in range(len(random_winning())):
         if computer_random_ticket()[i] == random_winning()[i]:
+            i += 1
+            return i
+        else:
+            return 0
+print(compare_tickets())
+
+def gambling_problem():
+    matching_picks = {
+        1 : 4,
+        2 : 7,
+        3 : 100,
+        4 : 50000,
+        5 : 1000000,
+        6 : 25000000
+    }
+
+    for key in matching_picks.keys():
+        if compare_tickets() == key:
+            winnings = matching_picks[key]
+            return f"You won {winnings}!"
+        else:
+            return "No winner"
+print(gambling_problem())
