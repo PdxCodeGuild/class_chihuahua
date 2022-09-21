f443ca8667e40eea40e4eacd167affa977d6965c
No Go <isabel.coverdale@yahoo.com>
2022-09-20 23:18:14 -0500

    still in progress count word

--- code/isabel/Python/labs/count_words.py
+++ code/isabel/Python/labs/count_words.py
@@ -1,6 +1,6 @@
 with open('hyde10.txt', 'r') as book:
     contents = book.read()
-    print(contents) #type str
+    #print(contents) #type str
 
 
     # words_counted = {
@@ -9,13 +9,47 @@ with open('hyde10.txt', 'r') as book:
 
     lowercase_hyde = contents.lower()
     # print(lowercase_hyde)
-    for char in lowercase_hyde:
-        no_space = lowercase_hyde.split(" ")
-        no_space = str(no_space)
+    no_space = lowercase_hyde.split("' '.!?()--'")
+    # no_space = str(no_space)
     print(no_space)
-    for char in no_space:
-        no_period = no_space.split(".")
-        no_period = str(no_period)
-    print(no_period)
+    # no_period = no_space.split(".!,?()--")
+    # # no_period = str(no_period)
+    # print(type(no_period))
+    # no_comma = no_period.split(",")
+    # no_comma = str(no_comma)
+    # # print(no_comma)
+    # no_exclam = no_comma.split("!")
+
+    # def convert_to_str(ls):
+    #     return str(ls)
+
+    # def no_spaces():
+    #     no_space = lowercase_hyde.split(" ")
+    #     no_space = convert_to_str(no_spaces)
+    #     return no_space
+    # print(type(no_spaces()))
+
+    # def no_periods():
+
+    #     no_period = no_spaces.split(",!.?()--")
+    #     no_period = convert_to_str(no_period)
+    #     return no_period
+    # print(no_periods())
+        
+    
+
+    
+    
+    # def format_hyde():
+    #     lowercase_hyde = contents.lower() #str
+    #     no_space = lowercase_hyde.split(" ") #list
+    #     no_space = str(no_space)
+    #     no_period = no_space.split(".")
+    #     no_comma = no_period
+        
+    # for char in no_space:
+    #     no_period = no_space.split(".")
+    #     no_period = str(no_period)
+    # print(no_period)
 
     
