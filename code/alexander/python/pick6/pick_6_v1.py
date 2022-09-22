6e706e13d920d6b535d3a6bc2b83d6a90346c8b9
User <user@iMac.local>
2022-09-21 21:14:48 -0700

    pick 6 v1

--- code/alexander/python/pick6/pick_6_v1.py
+++ code/alexander/python/pick6/pick_6_v1.py
@@ -1,35 +1,31 @@
-# pick 6 random integers as the 'winning number'
 import random
-# use randint
-def pick6():
-    winning_number = [random.randint(1,100) for num in range(6)]
-    return winning_number
 
-ticket_quantity = 0
-balance = 0
-def ticket():
-    while ticket_quantity < 100,000
+winning_number = [random.randint(1,10) for num in range(6)]
+
+money_count = {
+    0 : 0,
+    1 : 4,
+    2 : 7,
+    3 : 100,
+    4 : 50000,
+    5 : 1000000,
+    6 : 25000000
+}
+
+def num_matches():
+    balance = 0
+    ticket_quantity = 0
+    grand_total = 0
+    while ticket_quantity < 100000:
         ticket_generated = [random.randint(1,100) for num in range(6)]
-        ticket_quantity += 1
         balance -= 2
-        return ticket_generated
-
-def compare(pick6(),ticket()):
-    
-
-# balance at zero
-
-
-def num_matches(pick6,ticket):
-    if pick6 == ticket:
-        return
-    pass
-# ticket cost $2
-'''
-1 number match $4
-2 number match $7
-3 number match $100
-4 number match $50,000
-5 number match $1,000,000
-6 number match $25,000,000
-'''
+        counter = 0
+        for char in range(len(winning_number)):
+            if winning_number[char] == ticket_generated[char]:
+                counter += 1
+        print(counter)
+        grand_total += money_count[counter]
+        counter = 0 
+        ticket_quantity += 1 
+        print(balance)
+num_matches()
