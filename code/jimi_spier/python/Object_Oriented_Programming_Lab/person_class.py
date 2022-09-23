52897982d2cd63bbcd08f5ed4b8a73ca07fbb5b6
autobotJimi <jimi.spier@gmail.com>
2022-09-22 21:57:22 -0700

    finished rectangle_class with output

---
+++ code/jimi_spier/python/Object_Oriented_Programming_Lab/person_class.py
@@ -0,0 +1,19 @@
+
+# Jimi Spier
+# person_class.py
+# 20220922
+
+"""
+
+    Create a Python class Person with attributes: name and age.
+    Create a display() method that displays the name and age of an object created via the Person class.
+    Create a child class Student which inherits from the Person class and which also has a section attribute.
+    Create a method display_student() that displays the name, age and section of an object created via the Student class.
+
+
+
+
+
+"""
+class Person:
+    pass
