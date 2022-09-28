60dbf61402dd6fe3d8e3d6a53973695826d75586
No Go <isabel.coverdale@yahoo.com>
2022-09-27 23:13:52 -0500

    in progress part 2 dad joke

--- code/isabel/Python/labs/api_requests/dad_joke_part2.py
+++ code/isabel/Python/labs/api_requests/dad_joke_part2.py
@@ -4,15 +4,16 @@ import time
 url = 'https://icanhazdadjoke.com/'
 terms = 'hipster'
 
-response = requests.get(url, params={"search_term":"{terms}"}, headers ={'Accept':'application/json'})
+# response = requests.get(url, params={"search_term":{terms}}, headers ={'Accept':'application/json'})
 # data = response.json()
 # print(data)
-# response = requests.get(f'https://icanhazdadjoke.com/?search_term={terms}', headers ={'Accept':'application/json'})
+response = requests.get(f'https://icanhazdadjoke.com/search', headers ={'Accept':'application/json'}, params={"term":terms})
 data = response.json()
 
 
-print(data)
 
-for word in data['joke']:
-    if word == terms:
-        print(data['joke'])
+# for word in data['joke']:
+#     if word == terms:
+#         print(data['joke'])
+
+print(data)
