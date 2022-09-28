d5a88d9962d0213290a8bcdf68ff8b4bc6f29fc9
Jeff Helmer <jeff_helmer@yahoo.com>
2022-09-27 20:49:21 -0700

    add weather api 1 and 2 labs

--- code/jeff/Python/weather_api_lab.py
+++ code/jeff/Python/weather_api_lab.py
@@ -1,6 +1,7 @@
 import requests
 
-response =  requests.get('https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={884cfd64f3a52a3354c76c381207cf1e}')
+city_name = input('\nPlease enter the city name\n')
+response =  requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=884cfd64f3a52a3354c76c381207cf1e')
 data = response.json()
 
-print(data)
+print(data['main'])
