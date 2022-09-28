7fc47553a2a5ec6dba27a08f860908d937c32708
No Go <isabel.coverdale@yahoo.com>
2022-09-27 23:33:57 -0500

    dad joke part 2

--- code/isabel/Python/labs/api_requests/dad_joke_part2.py
+++ code/isabel/Python/labs/api_requests/dad_joke_part2.py
@@ -1,19 +1,22 @@
 import requests
 import time
 
-url = 'https://icanhazdadjoke.com/'
-terms = 'hipster'
+url = 'https://icanhazdadjoke.com/search'
+# terms = 'hipster'
 
-# response = requests.get(url, params={"search_term":{terms}}, headers ={'Accept':'application/json'})
+# # response = requests.get(url, headers ={'Accept':'application/json'}, params={"term":terms})
+
+# response = requests.get(f'https://icanhazdadjoke.com/search', headers ={'Accept':'application/json'}, params={"term":terms})
 # data = response.json()
+
 # print(data)
-response = requests.get(f'https://icanhazdadjoke.com/search', headers ={'Accept':'application/json'}, params={"term":terms})
-data = response.json()
 
-
-
-# for word in data['joke']:
-#     if word == terms:
-#         print(data['joke'])
-
-print(data)
+while True:
+    ask_user = input('Please enter a search word: ')
+    terms = ask_user
+    response = requests.get(f'https://icanhazdadjoke.com/search', headers ={'Accept':'application/json'}, params={"term":terms})
+    data = response.json()
+    # for i in data['results']:
+    #     if data[i]['joke'].find(terms) == terms: 
+    # print(data['results'][1]['joke'])
+    print(data['results'])
