a3a604622eee8a97a0596a5b230c25d299f2b212
hieul98 <hieule1998@yahoo.com>
2022-09-28 18:36:18 -0700

    completed weather API both versions

--- code/Hieu_L/python/weather_api.py
+++ code/Hieu_L/python/weather_api.py
@@ -1,5 +1,6 @@
 import requests
 #----------- v1 displays weather
+# zip_code = input('enter zip code: ')
 def lat_and_long(zip_code):
     response = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code}&appid=884cfd64f3a52a3354c76c381207cf1e')
     data = response.json()
@@ -12,7 +13,13 @@ def current_weather(zip):
     data = response.json()
     return data
 
-# zip_code = input('enter zip code: ')
+def three_day_forecast(zip):
+    lat = zip['lat']
+    lon = zip['lon']
+    response =  requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=884cfd64f3a52a3354c76c381207cf1e')
+    data = response.json()
+    return data
+
 # zip = lat_and_long(zip_code)
 # weather = current_weather(zip)
 # print(weather)
@@ -20,26 +27,19 @@ def current_weather(zip):
 
 #-------------- v2 asking for specific outputs
 
-choice = input('enter 1 for current weather \nenter 2 for 3-hour forecast \n')
-def three_day_forecast(zip):
-    lat = zip['lat']
-    lon = zip['lon']
-    response =  requests.get(f'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=884cfd64f3a52a3354c76c381207cf1e')
-    data = response.json()
-    return data
+choice = input('enter one for current weather \nenter two for 3-hour forecast \n')
 
 def weather_api(choice):
-    if choice == 1:
+    if choice == 'one':
         zip_code = input('enter zip code: ')
         zip = lat_and_long(zip_code)
         weather = current_weather(zip)
-        print(weather)
-    elif choice == 2:
+        return weather
+    else:
         zip_code = input('enter zip code: ')
         zip = lat_and_long(zip_code)
         weather = three_day_forecast(zip)
-        print(weather)
+        return weather
 
-# choice = input('enter 1 for current weather \n enter 2 for 3-hour forecast \n')
 weather = weather_api(choice)
 print(weather)
