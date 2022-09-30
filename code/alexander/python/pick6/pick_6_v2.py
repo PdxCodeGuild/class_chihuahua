ddccb0c536518dddc983a252b47c87cd12abc320
User <user@iMac.local>
2022-09-29 19:18:21 -0700

    I changed line 29 to match the equation given in version two instructions. I changed blance to expenses. grand total is earnings.

--- code/alexander/python/pick6/pick_6_v2.py
+++ code/alexander/python/pick6/pick_6_v2.py
@@ -12,12 +12,12 @@ money_count = {
 
 def num_matches():
     winning_number = [random.randint(1,100) for num in range(6)]
-    balance = 0
+    expenses = 0
     ticket_quantity = 0
     grand_total = 0
     while ticket_quantity < 100000:
         ticket_generated = [random.randint(1,100) for num in range(6)]
-        balance -= 2
+        expenses -= 2
         counter = 0
         for char in range(len(winning_number)):
             if winning_number[char] == ticket_generated[char]:
@@ -26,7 +26,7 @@ def num_matches():
         counter = 0 
         ticket_quantity += 1
 
-    return_on_investment = grand_total + balance
-    print(f"you spent {balance} on tickets.\nyou made {grand_total} in winnings.\nyour ROI is {return_on_investment}.\ndon't quit your day job :)")
+    return_on_investment = (grand_total + expenses) / expenses #expenses is a negative number so I just used the plus sign in the equation
+    print(f"you spent {expenses} on tickets.\nyou made {grand_total} in winnings.\nyour ROI is {return_on_investment}.\ndon't quit your day job :)")
 
 num_matches()
