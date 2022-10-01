0d238d47e152b3fe7251a54694d22e9d8ccdbb3c
Dani Cantu <daniellecantu@dovahkiin.myfiosgateway.com>
2022-10-01 00:04:24 -0400

    updating pick6

--- code/danielle/Python/pick6_2.py
+++ code/danielle/Python/pick6_2.py
@@ -6,14 +6,21 @@ def pick6():
     for i in range(n):
         winning.append(random.randint(1,100))
     print(winning)
-    ticket = input("Choose 6 numbers (1-99), separated by a comma and space: ")
+    ticket = []
+    n = 6
+    for i in range(n):
+        ticket.append(random.randint(1,100))
     print(ticket)
 
     def num_matches(winning, ticket):
-        for winning in ticket:
-            if winning == ticket:
+        while ticket in winning:
+            if ticket[0] == winning[0]:
+                print("You have 1 match")
+            elif ticket[0, 1] == winning[0, 1]:
+                print("You have 2 matches")
+            elif winning == ticket:
                 print("Your ticket matches the winning ticket")
     
-    num_matches(winning, ticket)
+        num_matches(winning, ticket)
 
 pick6()
