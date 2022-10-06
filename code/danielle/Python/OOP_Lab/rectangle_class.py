d250a8f94da2aff1c35839d4ed43c82eb588099e
Dani Cantu <112992437+dlcantu@users.noreply.github.com>
2022-10-06 00:14:26 -0400

    Update rectangle_class.py
    Still working on this...

--- code/danielle/Python/OOP_Lab/rectangle_class.py
+++ code/danielle/Python/OOP_Lab/rectangle_class.py
@@ -1,12 +1,11 @@
-class Rectanlge:
+class Rectangle:
     def perimeter(length, width):
         perimeter = int(length) + int(width)
     def area(length, width):
         area = int(length) * int(width)
-    def display():
         
+Measurement = Rectangle
+Measurement.perimeter(5, 6)
+Measurement.area(4, 5)
 
-# Write a Rectangle class in Python language, allowing you to build a rectangle with length and width attributes.
-# Create a perimeter() method to calculate the perimeter of the rectangle and a area() method to calculate the area of ​​the rectangle.
-# Create a method display() that display the length, width, perimeter and area of an object created using an instantiation on rectangle class.
-# Create a Parallelepipede child class inheriting from the Rectangle class and with a height attribute and another volume() method to calculate the volume of the Parallelepiped.
+print(Measurement)
