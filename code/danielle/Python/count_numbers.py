60a6e6bf153a7c4d4e154c34027d1c83e0e19cda
Dani Cantu <112992437+dlcantu@users.noreply.github.com>
2022-10-05 23:07:20 -0400

    Update count_numbers.py

--- code/danielle/Python/count_numbers.py
+++ code/danielle/Python/count_numbers.py
@@ -3,4 +3,9 @@ import string
 response = requests.get('https://www.gutenberg.org/files/62897/62897-0.txt')
 response.encoding = 'utf-8' # set encoding to utf-8
 punctuation_removal = str.maketrans('', '', string.punctuation)
-print(response.text.lower().translate(punctuation_removal))
+# print(response.text.lower().translate(punctuation_removal).split())
+
+words = list(response.text.lower().translate(punctuation_removal).split())
+words.sort(key=lambda tup: tup[0], reverse=True) 
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])
