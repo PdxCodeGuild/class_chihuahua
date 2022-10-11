5940d86448848a99b6adb027ab1437bb46165c6a
Dani Cantu <112992437+dlcantu@users.noreply.github.com>
2022-10-10 22:29:50 -0400

    Update bank_account_class.py
    OOP Bank Account lab should be complete

--- code/danielle/Python/OOP_Lab/bank_account_class.py
+++ code/danielle/Python/OOP_Lab/bank_account_class.py
@@ -1,4 +1,3 @@
-
 class Bank_Account: 
     def __init__(self, account_number, name, balance):
         self.account_number = account_number
@@ -6,19 +5,12 @@ class Bank_Account:
         self.balance = balance
     def deposit(self, amount):
         self.balance += amount
-        print(f"Your current balance is {self.balance}")
     def withdrawl(self, amount):
         if amount > self.balance:
             print("Insufficient funds...")
-    def bankFees(self):
-        print(f"Current bank fees asociated with your account are {(self.balance) * .05}")
-    def display():
-        print(f"{Bank_Account}")
+    def display(self):
+        print(f"Details for account {self.account_number}, {self.name}: Your current balance is {self.balance}")
 
-Dani_account = Bank_Account("XXX-XX-3289", "Danielle C.", 345)
-Dani_account.deposit(20)
-Dani_account.withdrawl(40)
-Dani_account.bankFees()
-Dani_account.display
+display = Bank_Account("XXX-XX-3289", "Danielle C.", 345)
 
-print(Dani_account)
+display.display()
