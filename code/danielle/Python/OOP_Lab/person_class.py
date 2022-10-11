8dfae8adca5c85bbb74e53515c048067e9300ed2
Dani Cantu <112992437+dlcantu@users.noreply.github.com>
2022-10-10 22:06:23 -0400

    Update person_class.py
    Person class should be done

--- code/danielle/Python/OOP_Lab/person_class.py
+++ code/danielle/Python/OOP_Lab/person_class.py
@@ -2,9 +2,19 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        def display():
-            print(f"Your name is {name} and your age is {age}")
-        display()
-
+    def display(self):
+            print(f"Your name is {self.name} and your age is {self.age}")
+            
 class Student(Person):
-    def __init__(self):
+    def __init__(self, name, age, section):
+        self.name = name
+        self.age = age
+        self.section = section
+    def display(self):
+        print(f"Your name is {self.name}, age: {self.age}, in section: {self.section}")
+
+display = Person("Danielle", 27)
+display_student = Student("Danielle", 27, 1)
+
+display.display()
+display_student.display()
