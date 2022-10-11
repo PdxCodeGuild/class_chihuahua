2faf7c8feeaa3bdfc0ecf5c0bbb58b05612a007b
Dani Cantu <112992437+dlcantu@users.noreply.github.com>
2022-10-10 22:01:43 -0400

    Update rectangle_class.py
    Rectangle OOP is complete

--- code/danielle/Python/OOP_Lab/rectangle_class.py
+++ code/danielle/Python/OOP_Lab/rectangle_class.py
@@ -1,11 +1,27 @@
 class Rectangle:
-    def perimeter(length, width):
-        perimeter = int(length) + int(width)
-    def area(length, width):
-        area = int(length) * int(width)
-        
-Measurement = Rectangle
-Measurement.perimeter(5, 6)
-Measurement.area(4, 5)
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+    def perimeter(self):
+        perimeter = 2 * ((self.length) + (self.width))
+        print(f"perimeter = {perimeter}")
+    def area(self):
+        area = (self.length) * (self.width)
+        print(f"area = {area}")
 
-print(Measurement)
+
+class Parallelepipede(Rectangle):
+    def __init__(self, width, length, height):
+        self.width = width
+        self.length = length
+        self.height = height
+    def volume(self):
+        volume = (self.width * self.length) * self.height
+        print(f"volume = {volume}")
+        
+display = Rectangle(5, 6)
+display = Parallelepipede(5, 6, 2)
+
+display.area()
+display.perimeter()
+display.volume()
