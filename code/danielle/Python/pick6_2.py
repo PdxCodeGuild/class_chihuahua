18f727d950c7d908f399c197e1b1a238180b167c
Dani Cantu <112992437+dlcantu@users.noreply.github.com>
2022-10-11 23:24:07 -0400

    Update pick6_2.py
    Updating pick6 - still not complete yet.

--- code/danielle/Python/pick6_2.py
+++ code/danielle/Python/pick6_2.py
@@ -3,14 +3,18 @@ import random
 def pick6():
     winning = []
     n = 6
+    num = 5
     for i in range(n):
         winning.append(random.randint(1,100))
-    print(winning)
+        while num < 10:
+            print(winning)
     ticket = []
     n = 6
     for i in range(n):
         ticket.append(random.randint(1,100))
     print(ticket)
+    
+
 
     def num_matches(winning, ticket):
         while ticket in winning:
@@ -18,6 +22,12 @@ def pick6():
                 print("You have 1 match")
             elif ticket[0, 1] == winning[0, 1]:
                 print("You have 2 matches")
+            elif ticket[0, 1, 2] == winning[0, 1, 2]:
+                print("You have 3 matches")
+            elif ticket[0, 1, 2, 3] == winning[0, 1, 2, 3]:
+                print("You have 4 matches")
+            elif ticket[0, 1, 2, 3, 4] == winning[0, 1, 2, 3, 4]:
+                print("You have 5 matches")   
             elif winning == ticket:
                 print("Your ticket matches the winning ticket")
     
