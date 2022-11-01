f43eacd1179669d65d0ec9f37a4502fac0d7c20c
ic <64978735+YugioCard59@users.noreply.github.com>
2022-10-31 23:25:14 -0500

    correcting todo models.py

--- code/isabel/Lab_Django/todo_app/models.py
+++ code/isabel/Lab_Django/todo_app/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class TodoItem:
-    def __init__(self, text, start_date, end_date, completion_check):
-        self.text = text
-        self.start_date = start_date
-        self.end_date = end_date
-        self.completion_check = completion_check
-        
+# class TodoItem:
+#     def __init__(self, text, start_date, end_date, completion_check):
+#         self.text = text
+#         self.start_date = start_date
+#         self.end_date = end_date
+#         self.completion_check = completion_check
+class TodoItem(models.Model):
+    text = models.CharField(max_length = 300)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    completion_check = models.BooleanField()
+
