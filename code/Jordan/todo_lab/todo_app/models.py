ab8d4430972b502eae46d043a6f81d3276c65aba
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-11-01 20:57:34 -0500

    template created

---
+++ code/Jordan/todo_lab/todo_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Blog(models.Model):
+        title = models.CharField(max_length = 200)
+        text = models.TextField(max_length = 500)
+        pub_date = models.DateField()
+
+        def __str__(self):
+            return self.title
---
+++ code/Jordan/todo_lab/todo_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('about/', views.about, name = 'about'),
+    path('posts/', views.blog_posts, name = 'posts'),
+    path('add/', views.add_post, name = 'add_posts'),
+    path('delete_post/<int:id>', views.delete_post, name='delete_post'),
+    path('details/<int:id>', views.see_details, name = 'see_details'),
+    path('update_post/<int:id>', views.update_post, name = 'update_post')
+
+
+]
