552baa8aa36bbe1d39410f188349098b5eb0068a
ic <64978735+YugioCard59@users.noreply.github.com>
2022-11-11 22:24:31 -0600

    continuing shorturl lab using solutions as guide

--- code/isabel/django_shorturl/url_shortener_project/urls.py
+++ code/isabel/django_shorturl/url_shortener_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(url_shortener_app.urls))
+    path('', include('url_shortener_app.urls'))
 ]
+# don't forget quotes inside include()
