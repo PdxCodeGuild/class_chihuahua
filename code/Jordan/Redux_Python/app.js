5df0c93257abf41f1534e235d05115dbdb3f7f18
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-11-21 23:20:00 -0600

    version 1

---
+++ code/Jordan/Redux_Python/app.js
@@ -0,0 +1,7 @@
+const ft = 0.3048
+// const mi = 1609.34
+// const m = 1
+// const km = (m * 1000)
+let inputUnit = prompt("What is the distance in feet?");
+let outPutUnit = inputUnit * ft
+alert(inputUnit +" feet " + " is " + outPutUnit + " meters ")
