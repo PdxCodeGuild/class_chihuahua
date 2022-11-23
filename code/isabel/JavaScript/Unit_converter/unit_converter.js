bd71f191d401016fe35af23e8c7a7742d3fb82dc
ic <64978735+YugioCard59@users.noreply.github.com>
2022-11-22 20:09:20 -0600

    Version 1 Unit Converter

--- code/isabel/JavaScript/Unit_converter/unit_converter.js
+++ code/isabel/JavaScript/Unit_converter/unit_converter.js
@@ -1,33 +1,47 @@
-/*const unit_converter = {
-    ask_user: function(){
-        let user_number = Number(prompt('Enter a number to be converted from feet to meters:  '))
-        return user_number
-    },
-    convertFeetToMeters() {
-        return unit_converter.ask_user * 0.3048
-    },
-    answerAskUser() {
-        return `${unit_converter.ask_user[0]} feet is ${unit_converter.convertFeetToMeters()} meters.`
-    }
-}
-
-console.log(unit_converter.ask_user[0])
-console.log(unit_converter.answerAskUser())
-*/
-
 const unit_converter = {
-    askUser(){
-        let user_number = Number(prompt('Enter a number to be converted from feet to meters:  '))
-        return user_number
+    askUser() {
+      let user_number = Number(prompt('Enter a number to be converted from feet to meters:  '))
+      return user_number
     },
     convertFeetToMeters() {
-        return unit_converter.askUser * 0.3048
+      let conversion = unit_converter.askUser() * 0.3048
+      return conversion
+  
     },
     answerAskUser() {
-        return `${unit_converter.askUser} feet is ${unit_converter.convertFeetToMeters()} meters.`
+      return `${unit_converter.askUser()} feet is ${unit_converter.convertFeetToMeters()} meters.`
     }
-}
+  }
+  
+  console.log(unit_converter.answerAskUser())
+/*above works*/
 
-console.log(unit_converter.askUser())
-console.log(unit_converter.answerAskUser())
+// const unit_converter = {
+//     askUser() {
+//       let user_number = Number(prompt('Enter a number to be converted:  '))
+//       return user_number
+//     },
+//     askUserStartUnit() {
+//         let start_unit = prompt('Enter units to be converted in: m, mi, ft, yd, or km:  ')
+//         return start_unit
+//     },
+//     askUserConversion() {
+//         let user_conversion = prompt('Enter units to convert to in: m, mi, ft, yd, or km:  ')
+//         return user_conversion
+//     },
+//     convertFromTo() {
+//         if unit_converter.askUserStartUnit() === 'm' && unit_converter.askUserConversion() === 'ft' 
+//             let conversion = unit_converter.askUser() * 0.3048
+//             return conversion
+  
+//     },
+//     answerAskUser() {
+//       return `${unit_converter.askUser()} feet is ${unit_converter.convertFeetToMeters()} meters.`
+//     }
+//   }
+  
+//   console.log(unit_converter.answerAskUser())
+  
+
+  
 
