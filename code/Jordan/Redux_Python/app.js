83657d4a45f2109f819b6591954becdedd918b7e
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-11-23 23:17:14 -0600

    unit converter final

--- code/Jordan/Redux_Python/app.js
+++ code/Jordan/Redux_Python/app.js
@@ -7,14 +7,25 @@ const unitConverter = {
     inch: 0.0254
 };
 
-const ft = 0.3048
-const mi = 1609.34
-const m = 1
-const km = (m * 1000)
-const yard = .9144
-const inch = .0254
 let inputUnit = prompt("What is the distance?");
-let startingUnit = prompt("What is the conversion units you'd like to start with? ft, yard, inch, mi, m, or km?")
-let askUnit = prompt("What units would you like to convert? ft, yard, inch, mi, m, or km?");
-let outPutUnit = unitConverter[askUnit] * Number(inputUnit);
-alert(Number(inputUnit) + askUnit + " is " + outPutUnit + " meters ")
+let startingUnit = prompt("What is the conversion units you'd like to start with? Choose either ft, yard, inch, mi, m, or km.");
+let askUnit = prompt("What units would you like to convert to? ft, yard, inch, mi, m, or km?");
+if (startingUnit == askUnit) {
+    outPutUnit = inputUnit;
+}
+else {
+    distanceMeters = unitConverter[startingUnit] * Number(inputUnit);
+    outPutUnit = Math.round(distanceMeters / unitConverter[askUnit]);
+}
+alert(Number(inputUnit) + startingUnit + " is " + outPutUnit + askUnit)
+
+// function idea
+// if startingUnit == askUnit {
+//  let outPutUnit = startingUnit
+// }
+// else {
+//  outPutUnit = unitConverter[askUnit] * Number(inputUnit);    
+// }
+//                  return startingUnit
+//                  else if unitConverter[askUnit] * Number(inputUnit);
+// let outPutUnit = unitConverter[askUnit] * Number(inputUnit);
