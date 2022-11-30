b8475959f59524e0608d24c03e10e92fb2c5ba5c
ic <64978735+YugioCard59@users.noreply.github.com>
2022-11-29 23:04:00 -0600

    working on event listener, trying to remove text from input field

--- code/isabel/JavaScript/Todo/todo.js
+++ code/isabel/JavaScript/Todo/todo.js
@@ -1,1 +1,11 @@
 const textField = document.getElementById("thing_todo");
+const todo_item = document.getElementById("todo_item");
+const trigger = document.getElementById("trigger");
+let incompleteUl = document.getElementById("incomplete");
+
+trigger.addEventListener("click", function() {
+    // console.log(textField.value);
+    // incompleteUl.appendChild(textField.value);
+    incompleteUl.innerHTML = `<li>${textField.value}</li>`;
+
+});
