435eccb3120978f895d10fece387d27be3f65dac
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-11-30 20:08:59 -0600

    version 1 pick 6 complete

--- code/Jordan/Pick6_JavaScript/app.js
+++ code/Jordan/Pick6_JavaScript/app.js
@@ -1,22 +1,38 @@
-// function to create the winning numbers
-const ticket = 6;
-const winningNumbers = [];
-const earnings = {
-    0:0,
-    1:4, 
-    2:7, 
-    3:100, 
-    4:50000, 
-    5:1000000, 
-    6:25000000
-};
-
 function pick6() {
     let randomSix = []
-    for(let index = 0; index < 6; index += 1) {
-        randomSix.push(Math.floor(Math.random()*10)) // multiply by 10 to make it a whole number
+    for(let index = 0; index < 6; index ++) {
+        randomSix.push(Math.floor(Math.random()*100)) // multiply by 10 to make it a whole number
     }
-//console.log(randomSix)
-return randomSix
+    return randomSix
 };
-console.log(pick6())
+//console.log(pick6())
+
+function winnings(winningNumbers, playerNumbers) {
+    let matches = 0
+    const earnings = {
+        0:0,
+        1:4, 
+        2:7, 
+        3:100, 
+        4:50000, 
+        5:1000000, 
+        6:25000000
+    };
+    for(let index = 0; index < 6; index ++) {
+        if (winningNumbers[index] === playerNumbers[index]) {
+            matches ++
+        }
+    }
+    return earnings[matches]
+}
+
+let winningTicket = pick6()
+let playerBalance = 0
+let i = 0;
+while(i < 100000) {
+    let playerTicket = pick6()
+    playerBalance -=2 // Balance = Balance -2
+    playerBalance += winnings(winningTicket, playerTicket) 
+    i++;
+}
+console.log(playerBalance)
