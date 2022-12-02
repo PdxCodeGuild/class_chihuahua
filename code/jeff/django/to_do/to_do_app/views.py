b053b9041abeb641fbbd11f213bc9e8933f7af15
Jeff Helmer <jeff_helmer@yahoo.com>
2022-12-01 21:25:26 -0800

    pushing django todo, almost complete

--- code/jeff/django/to_do/to_do_app/views.py
+++ code/jeff/django/to_do/to_do_app/views.py
@@ -27,12 +27,31 @@ def new(request):
     elif request.method == "POST":
         title = request.POST['title']
         print(title)
+        title = request.POST['title']
         description = request.POST['description']
+        created_date = request.POST['created_date']
+        completed_date = request.POST['completed_date']
+        completed = request.POST['completed_check']
+        if completed == 'on': 
+            completed = True
+        TodoItem.objects.create(title=title, description=description, created_date=created_date, completed_date=completed_date, completed=completed)
         return redirect('new')
 
 
 
 
+
 def edit(request):
-    return render(request, 'pages/edit.html')
+    if request.method == "GET":   
+        return render(request, 'pages/edit.html')
+    elif request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        created_date = request.POST['created_date']
+        completed_date = request.POST['completed_date']
+        completed = request.POST['completed_check']
+        if delete == 'on': 
+            delete = True,
+        TodoItem.objects.delete(title=title, description=description, created_date=created_date, completed_date=completed_date, completed=completed)
+        title = request.POST['title']
 
