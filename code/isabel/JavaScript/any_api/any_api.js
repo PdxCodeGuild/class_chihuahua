10a7f7ae30e9b8f16b4f6161b60b75298efc9fdd
ic <64978735+YugioCard59@users.noreply.github.com>
2022-12-05 23:23:12 -0600

    first attempt any api lab

--- code/isabel/JavaScript/any_api/any_api.js
+++ code/isabel/JavaScript/any_api/any_api.js
@@ -1,14 +1,9 @@
-fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&media_type="image"')
+fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(function (response) {
         return response.json()
     }).then(function (data) {
-        console.log(json)
-        const dom_manipulation = document.getElementById("image");
         document.getElementById("image").src = data.hdurl
-        dom_manipulation.innerHTML = data.media_type;
-    
-        //write asynchronous code here
-
+        
     })
     .catch(function (error) {
         console.log(error)
