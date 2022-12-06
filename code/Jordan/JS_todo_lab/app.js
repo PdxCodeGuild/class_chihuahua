0831cc7d94a8ce3091feeb34223eeeadc9e13aa2
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-12-05 23:31:59 -0600

    functional but needs CSS

--- code/Jordan/JS_todo_lab/app.js
+++ code/Jordan/JS_todo_lab/app.js
@@ -1,26 +1,40 @@
-// let todoItems = []; 
-
-// function addTodo(text)
+let completedList = document.querySelector('#completed')
+let appendList = document.querySelector('#todo')
 let addButton = document.querySelector('#task-submit');
 addButton.addEventListener('click', function() { //when the user clicks the add button, it will run the function
     let userInput = document.querySelector('#new-task-input').value
-    let appendList = document.querySelector('#todo')
-
     let newTodo = document.createElement('li') //creating an html list element
-    newTodo.innerHTML = `${userInput}
-    <button class="complete">Complete</button> 
-    <button class="remove">Remove</button>` //on the inside of the li, insert this stuff
-    appendList.appendChild(newTodo) //creating an object so we can access them later
 
-    // let removeButton = document.querySelectorAll('.remove');
-    // for(i in removeButton) {
-    //     removeButton[i].addEventListener('click', function() {
-    //     }) 
-    // }
-    let completeButton = document.querySelectorAll('.complete');
-    for(i in completeButton) {
-        completeButton[i].addEventListener('click', function(){
-            console.log(appendList[0])
+    let deleteButton = document.createElement('button') // creating a delete button
+    deleteButton.classList.add('delete')
+    deleteButton.innerText = 'Delete' // the content within the tags, only accepts strings
+
+    let completeButton = document.createElement('button') // creating a complete button
+    completeButton.classList.add('complete')
+    completeButton.innerText = 'Complete'
+
+    newTodo.innerHTML = `${userInput}`
+    newTodo.appendChild(completeButton)
+    newTodo.appendChild(deleteButton)
+
+    appendList.appendChild(newTodo) 
+    completeButton.addEventListener('click', function() {
+        appendList.removeChild(newTodo)
+        let deleteComplete = document.createElement('button')
+        deleteComplete.classList.add('delete-complete')
+        deleteComplete.innerText = 'Delete'
+
+        let completedTodo = document.createElement('li')
+        completedTodo.innerText = userInput
+        completedTodo.appendChild(deleteComplete)
+        completedList.appendChild(completedTodo)
+        deleteComplete.addEventListener('click', function() {
+            completedList.removeChild(completedTodo)
         })
-    }
+    })
+    deleteButton.addEventListener('click', function() {
+        appendList.removeChild(newTodo)
+    })
+    
+
 } )
