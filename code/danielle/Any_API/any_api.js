1c2402e6e187782e9683420c4bc6489f601f7f8e
Dani Cantu <daniellecantu@dovahkiin.myfiosgateway.com>
2022-12-09 19:56:09 -0500

    updating

--- code/danielle/Any_API/any_api.js
+++ code/danielle/Any_API/any_api.js
@@ -1,9 +1,29 @@
-fetch('https://api.chucknorris.io/')
-    .then(function (response) {
-        return response.json()
-    }).then(function (data) {
-        //asynch code here
+const btn = document.getElementById('btn')
+const numberTrivia = document.getElementById('numberInput')
+let banner = document.getElementById('banner')
+let qualityBanner = document.getElementById('quality')
+
+btn.addEventListener('click', function () {
+    const number = numberTrivia.value
+    if (number == '') {
+        return
+    }
+    getData(number)
+
     })
-    .catch(function (error) {
-        console.log(error)
-    })
+
+    function getData(number) {
+        fetch(`http://numbersapi.com/${number}/trivia`) // ${}
+            .then(function (response) {
+                console.log(response)
+                return response.text()
+            })
+            .then(function (data) {
+                console.log(data)
+            })
+            .catch(function (error) {
+                console.log(error)
+                return error
+            })
+
+    }
