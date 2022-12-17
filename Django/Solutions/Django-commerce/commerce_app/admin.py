2de4e26ea935a020b4981ca3c4ed926d10795ab5
Alex De Michieli <alexdemichieli@Alexs-MacBook-Pro.local>
2022-12-16 21:33:17 -0800

    adding model admin modification

--- Django/Solutions/Django-commerce/commerce_app/admin.py
+++ Django/Solutions/Django-commerce/commerce_app/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import *
 
+class MyModelAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        print("HELLO",qs)
+        print(request.user)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "author":
+            kwargs["queryset"] = User.objects.filter(username=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Cart)
-admin.site.register(Product)
+admin.site.register(Product, MyModelAdmin)
 
 # Register your models here.
--- Django/Solutions/Django-commerce/commerce_app/models.py
+++ Django/Solutions/Django-commerce/commerce_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
@@ -12,6 +13,8 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
     price = models.IntegerField()
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    
 
     def __str__(self):
         return self.name
