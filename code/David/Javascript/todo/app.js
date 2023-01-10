c7e636f744a87fc8b7406ec87f6c8aa159515945
David Herrera <david.m.herrera1989@gmail.com>
2023-01-10 11:46:05 +0900

    fixed issues and completed todo JS app

--- code/David/Javascript/todo/app.js
+++ code/David/Javascript/todo/app.js
@@ -1,14 +1,37 @@
-const btn1 = document.getElementById('btn')
-const todoList = document.getElementById('todo')
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+const completedList = document.getElementById('completed-list');
 
+todoForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-// const textnode = document.createTextNode(testField)
+  const todoText = todoInput.value;
+  todoInput.value = '';
 
-btn1.addEventListener('click', function(){
-    const testField = document.getElementById('text').value
-    const liNode = document.createElement('LI')
-    liNode.innerHTML=testField
-    todoList.appendChild(liNode)
-    // console.log('hello')  
+  const todoItem = {
+    text: todoText,
+    completed: false,
+  };
 
-})
+  const todoEl = document.createElement('li');
+  todoEl.innerText = todoItem.text;
+  todoList.appendChild(todoEl);
+
+  const completeButton = document.createElement('button');
+  completeButton.innerText = 'Complete';
+  todoEl.appendChild(completeButton);
+
+  completeButton.addEventListener('click', () => {
+    todoItem.completed = true;
+  
+    // add a line through the completed to-do item
+    todoEl.style.textDecoration = 'line-through';
+  
+    // remove the "Complete" button
+    completeButton.remove();
+  
+    completedList.appendChild(todoEl);
+  });
+  
+});
