68bbf66ac540864bdcacb35ba90ec99406103427
hieul98 <hieule1998@yahoo.com>
2023-01-17 10:41:10 -0700

    last of the labs

--- code/Hieu_L/javascript/todo_list/todo.js
+++ code/Hieu_L/javascript/todo_list/todo.js
@@ -1,25 +1,36 @@
+const todos = ["task 1", "task 2",]
 let task = document.getElementById('todo_text')
 let add = document.getElementById('add')
 let list = document.getElementById('list')
 
 add.addEventListener("click", function (event) {
+    todos.push(task.value)
+    console.log(todos)
+});
+
+for (let i = 0; i < todos.length; i++) {
+    console.log(todos[i])
     let new_event = document.createElement('li');
     let complete_button = document.createElement("button");
-    new_event.innerHTML = task.value;
-    new_event.setAttribute("id","todo_event")
-    complete_button.setAttribute("id","complete_button");
+    new_event.setAttribute("id", "todo_event")
+    complete_button.addEventListener('click', function (event) {
+        task = new_event;
+        completed_area = document.getElementById("completed");
+        completed_area.appendChild(task);
+        console.log("works");
+        complete_button.remove();
+    });
+    new_event.innerHTML = todos[i];
     complete_button.innerHTML = 'complete';
     list.appendChild(new_event);
     list.appendChild(complete_button);
-});
-
-let complete = getElementById("complete_button")
-// let completed = getElementById('completed')
-
-complete_button.addEventListener('click', function (event) {
-    console.log("works")
-    // let todo_event = document.createElement('li');
-    // let task = document.getElementsByTagName('todo_event')
-    // todo_event.innerHTML = task.value;
-    // completed.appendChild(li);
-});
+    let delete_button = document.createElement("button");
+    delete_button.addEventListener("click", function (event) {
+        task = new_event
+        delete_button.remove();
+        delete todos[i];
+        console.log(todos);
+    });
+    delete_button.innerHTML = "delete";
+    list.appendChild(delete_button);
+};
