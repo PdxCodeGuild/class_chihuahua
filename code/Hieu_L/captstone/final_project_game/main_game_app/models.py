db3cb9b746beee71c59601a74a1895df2c23611a
hieul98 <hieule1998@yahoo.com>
2023-01-17 19:23:17 -0700

    presentation version of capstone project

--- code/Hieu_L/captstone/final_project_game/main_game_app/models.py
+++ code/Hieu_L/captstone/final_project_game/main_game_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class item(models.Model):
+    name = models.CharField(max_length=500)
+    defense = models.IntegerField()
+    attack = models.IntegerField()
+    image = models.ImageField(upload_to='main_game_app/static/')
+    def __str__(self):
+        return self.name
